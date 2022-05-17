d67d569a0cc10bbae095149e653b8541af253cbd
choipureum <pooreumsunny@gmail.com>
2022-05-17 23:26:16 +0900

    인터렉티브책 완성
    Signed-off-by: choipureum <pooreumsunny@gmail.com>

---
+++ example/08.swipe/js/main.js
@@ -0,0 +1,39 @@
+const swiper = new Swiper('.wrap', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction'
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    spaceBetween: 50,
+    slidesPerView: 'auto',
+    grabCursor: true,
+    centeredSlides: true,
+    speed: 1000,
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: -100,
+        depth: 400,
+        modifier: 1,
+        slideShadows: false,
+    },
+    autoplay: {
+        delay: 1000,
+        disableOnInteraction: true,
+    }
+});
+
+const btnStart = document.querySelector('.btnStart');
+const btnStop = document.querySelector('.btnStop');
+
+btnStart.addEventListener('click', () => {
+    swiper.autoplay.start();
+});
+btnStop.addEventListener('click', () => {
+    swiper.autoplay.stop();
+});
